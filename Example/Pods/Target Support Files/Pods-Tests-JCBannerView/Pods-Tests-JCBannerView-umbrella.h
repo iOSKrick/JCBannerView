afaa3ac7453e1034974b871ef2ce98e67fad48a9
lijingcheng <bj_lijingcheng@163.com>
2015-07-02 10:58:43 +0800

    no message

---
+++ Example/Pods/Target Support Files/Pods-Tests-JCBannerView/Pods-Tests-JCBannerView-umbrella.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+#import "JCBannerCell.h"
+#import "JCBannerView.h"
+
+FOUNDATION_EXPORT double JCBannerViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char JCBannerViewVersionString[];
+
