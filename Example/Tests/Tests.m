6aec22d2abfdcc08898ec2e7e70b0e6ba1fd8cdf
lijingcheng <jingcheng.li@mtime.com>
2016-02-15 10:42:05 +0800

    Revert "remove test target"
    This reverts commit d85c653fc47d9067a32807876615e20a06fdcd71.

---
+++ Example/Tests/Tests.m
@@ -0,0 +1,36 @@
+//
+//  JCBannerViewTests.m
+//  JCBannerViewTests
+//
+//  Created by lijingcheng on 07/02/2015.
+//  Copyright (c) 2015 lijingcheng. All rights reserved.
+//
+
+@import XCTest;
+@import UIKit;
+@import Foundation;
+
+@interface Tests : XCTestCase
+
+@end
+
+@implementation Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTAssert(YES, @"Pass");
+}
+
+@end
